a5f97abcdb9c66b184b477b00e7f118d1f3bc771
tanvir002700 <tanvir002700@gmail.com>
2017-10-15 20:07:38 +0600

    complete book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
-
+  mount_uploader :cover_photo, PhotoUploader
+  mount_uploader :pdf, PdfUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171015132830) do
-
-  create_table "attachments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string "file"
-    t.string "attachable_type"
-    t.bigint "attachable_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["attachable_type", "attachable_id"], name: "index_attachments_on_attachable_type_and_attachable_id"
-  end
+ActiveRecord::Schema.define(version: 20171014182244) do
 
   create_table "books", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
     t.text "description"
+    t.string "cover_photo"
+    t.string "pdf"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "cover_photo"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
