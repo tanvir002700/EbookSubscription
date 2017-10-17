98cc9663adfe57d871f8b9242df70eb399e9ca73
tanvir002700 <tanvir002700@gmail.com>
2017-10-18 02:02:28 +0600

    update rubocop and refactor code

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,9 +3,9 @@ class Book < ApplicationRecord
   has_and_belongs_to_many :subscriber, uniq: true, class_name: 'User', dependent: :destroy
   has_many :reviews
 
-  STATUS = [ :pending, :need_update, :approved, :rejected ]
+  STATUS = [:pending, :need_update, :approved, :rejected]
 
-  enum status: [ :pending, :need_update, :approved, :rejected ]
+  enum status: [:pending, :need_update, :approved, :rejected]
 
   mount_uploader :cover_photo, PhotoUploader
   mount_uploader :pdf, PdfUploader
