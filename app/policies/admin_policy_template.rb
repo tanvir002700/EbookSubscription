61eab1af3c5f045b621c9485fca57efdecdacc4b
tanvir002700 <tanvir002700@gmail.com>
2017-10-21 13:58:31 +0600

    add admin policy template

---
+++ app/policies/admin_policy_template.rb
@@ -0,0 +1,27 @@
+module AdminPolicyTemplate
+  def index?
+    admin_user?
+  end
+
+  def show?
+    admin_user?
+  end
+
+  def create?
+    admin_user?
+  end
+
+  def update?
+    admin_user?
+  end
+
+  def destroy?
+    admin_user?
+  end
+
+  private
+
+  def admin_user?
+    user.role == 'admin'
+  end
+end
