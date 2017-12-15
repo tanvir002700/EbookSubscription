afe7eb982c5e26eceb7c64e7b1b8a39485eb95ae
tanvir hasan <tanvir002700@gmail.com>
2017-12-16 00:31:52 +0600

    add book model test code

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#update_status' do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:book) { FactoryGirl.create(:book, user: user) }
+    it 'changes book status' do
+      book.update_status(:approved)
+      expect(Book.find(book.id).status).to eq('approved')
+    end
+  end
 end
