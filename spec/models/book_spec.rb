6d0d53035f6f72343c50e16c5433b894f37ee21e
tanvir hasan <tanvir002700@gmail.com>
2017-12-16 20:17:31 +0600

    add association test code for book model

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,6 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
+  describe 'associations' do
+    it 'belongs to user' do
+      expect(Book.reflect_on_association(:user).macro).to eq(:belongs_to)
+    end
+
+    it 'has and belongs to many subscriber' do
+      expect(Book.reflect_on_association(:subscriber).macro).to eq(:has_and_belongs_to_many)
+    end
+
+  end
+
   describe '#update_status' do
     let(:user) { FactoryGirl.create(:user) }
     let(:book) { FactoryGirl.create(:book, user: user) }
