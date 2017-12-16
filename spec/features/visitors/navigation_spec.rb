9b006f237daaac0b71c1209c026829b5f31a0709
tanvir hasan <tanvir002700@gmail.com>
2017-12-16 22:04:27 +0600

    remove some spec dir

--- spec/features/visitors/navigation_spec.rb
+++
@@ -1,8 +0,0 @@
-feature 'Navigation links', :devise do
-  scenario 'view navigation links for user' do
-    user = FactoryGirl.create(:user)
-    signin(user.email, user.password)
-    visit root_path
-    expect(page).to have_content 'Sign Out'
-  end
-end
--- spec/support/capybara.rb
+++
@@ -1,5 +0,0 @@
-Capybara.asset_host = 'http://localhost:3000'
-RSpec.configure do |config|
-  config.include Capybara::DSL
-  config.include Warden::Test::Helpers
-end
