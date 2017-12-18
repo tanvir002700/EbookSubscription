2fb2e22cad045349952a0c8be3de96c0a9bc37d2
tanvir hasan <tanvir002700@gmail.com>
2017-12-19 01:28:09 +0600

    add 3 type of user book permission spec

--- spec/policies/book_policy_spec.rb
+++ spec/policies/book_policy_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe BookPolicy do
 
   context 'When user is admin user' do
     let(:admin) { FactoryGirl.create(:admin) }
+
     permissions :index?, :new?, :create? do
       it 'grants access' do
         expect(subject).to permit(admin, Book.new)
@@ -20,6 +21,36 @@ RSpec.describe BookPolicy do
     permissions :edit?, :update?, :destroy? do
       it 'not grants access' do
         expect(subject).not_to permit(admin, book)
+      end
+    end
+  end
+
+  context 'When user is normal user' do
+    let(:user2) { FactoryGirl.create(:user) }
+
+    permissions :index?, :edit?, :update?, :destroy? do
+      it 'not grants access' do
+        expect(subject).not_to permit(user2, book)
+      end
+    end
+
+    permissions :subscribe?, :unsubscribe? do
+      it 'grants access' do
+        expect(subject).to permit(user2, book)
+      end
+    end
+  end
+
+  context 'When user is publisher' do
+    permissions :edit?, :update?, :destroy? do
+      it 'grants access' do
+        expect(subject).to permit(user, book)
+      end
+    end
+
+    permissions :index?, :subscribe?, :unsubscribe? do
+      it 'not grants access' do
+        expect(subject).not_to permit(user, book)
       end
     end
   end
