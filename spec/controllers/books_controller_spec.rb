fce7095d357d4ec743635513200bffb999c0bf9d
tanvir hasan <tanvir002700@gmail.com>
2017-12-20 21:26:55 +0600

    add test code for books controller

--- spec/controllers/books_controller_spec.rb
+++ spec/controllers/books_controller_spec.rb
@@ -42,4 +42,53 @@ RSpec.describe BooksController, type: :controller do
       end
     end
   end
+
+  describe '#uploaded_index' do
+    context 'when user is admin user' do
+      login_admin
+      it 'response successfully with an HTTP 200 status code' do
+        get :uploaded_index
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+        expect(response).to render_template :index
+      end
+    end
+
+    context 'when user is normal user' do
+      login_user
+      it 'response successfully with an HTTP 200 status code' do
+        get :uploaded_index
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+        expect(response).to render_template :index
+      end
+    end
+  end
+
+  describe '#subscribed_index' do
+    context 'when user is admin user' do
+      login_admin
+      it 'response successfully with an HTTP 200 status code' do
+        get :subscribed_index
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+        expect(response).to render_template :index
+      end
+    end
+  end
+
+  describe '#show' do
+    context 'when user is admin user' do
+      login_admin
+      xit 'response successfully with an HTTP 200 status code' do
+        get :show
+        puts "STAtus#################"
+        puts responnse.status
+        expect(response).to be_success
+        expect(response).to have_http_status(200)
+        #expect(response).to render_template :show
+      end
+    end
+  end
 end
+
